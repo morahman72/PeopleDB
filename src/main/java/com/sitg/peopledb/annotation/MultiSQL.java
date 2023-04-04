0ec1b75a8ab79b51814987f8ff69220bae92d3b0
morahman72 <morahman72@gmail.com>
2023-04-04 07:14:01 -0500

    New project 12-20 to push to GitHub

---
+++ src/main/java/com/sitg/peopledb/annotation/MultiSQL.java
@@ -0,0 +1,12 @@
+package com.sitg.peopledb.annotation;
+
+import java.lang.annotation.Annotation;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MultiSQL {
+    SQL[] value();
+
+
+}
---
+++ src/main/java/com/sitg/peopledb/model/Entity.java
@@ -0,0 +1,7 @@
+package com.sitg.peopledb.model;
+
+public interface Entity {
+    Long getId();
+
+    void setId(Long id);
+}
